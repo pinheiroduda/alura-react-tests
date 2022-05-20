f88aa52e23fa377a04158b6a97b6df3c4fe4323b
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-20 09:21:52 -0300

    feat: improves code organazition

--- src/App.test.js
+++ src/App.test.js
@@ -3,21 +3,23 @@ import { render, screen } from "@testing-library/react"
 import App from "./App"
 
 describe('Componente principal', () => {
-  it('Quando eu abro o app do banco, o nome é exibido', () => {
-    render(<App />);
-
-    expect(screen.getByText('ByteBank')).toBeInTheDocument()
-  })
-
-  it('Quando eu abro o app do banco, o saldo é exibido', () => {
-    render(<App />)
-    
-    expect(screen.getByText('Saldo:')).toBeInTheDocument()
-  })
-
-  it('Quando eu abro o app do banco, o botão de transação é exibido', () => {
-    render(<App />)
-    
-    expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+  describe('Quando eu abro o app do banco', () => {
+    it('o nome é exibido', () => {
+      render(<App />);
+  
+      expect(screen.getByText('ByteBank')).toBeInTheDocument()
+    })
+  
+    it('o saldo é exibido', () => {
+      render(<App />)
+      
+      expect(screen.getByText('Saldo:')).toBeInTheDocument()
+    })
+  
+    it('o botão de transação é exibido', () => {
+      render(<App />)
+      
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+    })
   })
 })
