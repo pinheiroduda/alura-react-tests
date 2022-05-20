d13185c60544f678701d023be15392a89b1b8053
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-20 13:54:23 -0300

    feat: test if transaction function is called when button is clicked

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Conta from './Conta'
 
 describe('Componente de conta', () => {
@@ -9,5 +9,16 @@ describe('Componente de conta', () => {
     const saldo = screen.getByTestId('saldo-conta')
 
     expect(saldo.textContent).toBe('R$ 1000')
+  })
+
+  it('Chama a função de realizar transação, quando o botão é clicado', () => {
+    const funcaoRealizarTransacao = jest.fn()
+
+    render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao}/>)
+
+    fireEvent.click(screen.getByText('Realizar operação'))
+
+    expect(funcaoRealizarTransacao).toHaveBeenCalled()
+
   })
 })
