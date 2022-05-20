01684e09b21f9397dc91add0f19c8703f8318475
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-20 10:53:53 -0300

    fat: test if a transaction is being made when a withdraw is made

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import App, { calcularNovoSaldo } from "./App"
 
 describe('Componente principal', () => {
@@ -33,6 +33,22 @@ describe('Componente principal', () => {
       const novoSaldo = calcularNovoSaldo(valores, 150)
 
       expect(novoSaldo).toBe(100)
+    })
+    it('que é um saque, a transação deve ser realizada', () => {
+      const {getByText, getByTestId, getByLabelText} = render(<App />)
+
+      const saldo = getByText('R$ 1000')
+      const transacao = getByLabelText('Saque')
+      const valor = getByTestId('valor')
+      const botaoTransacao = getByText('Realizar operação')
+
+      expect(saldo.textContent).toBe('R$ 1000')
+
+      fireEvent.click(transacao, {target: {value: 'saque'}})
+      fireEvent.change(valor, {target: {value: 10}})
+      fireEvent.click(botaoTransacao)
+
+      expect(saldo.textContent).toBe('R$ 990')
     })
   })
 })
