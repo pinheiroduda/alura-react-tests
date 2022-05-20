6b312581d6cc4f4adfefde20dec7c4f72ff44030
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-20 09:11:55 -0300

    feat: creates test to find App name

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+describe('Componente principal', () => {
+  it('Mostrar o nome do banco', () => {
+    render(<App />);
+
+    expect(screen.getByText('ByteBank')).toBeInTheDocument()
+  })
+})
