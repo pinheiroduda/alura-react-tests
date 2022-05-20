7b7da66761cf7f956790bd2ddf4592b436d1a265
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-20 09:34:56 -0300

    feat: test if the function is working when a withdrawal is made

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
-import App from "./App"
+import App, { calcularNovoSaldo } from "./App"
 
 describe('Componente principal', () => {
   describe('Quando eu abro o app do banco', () => {
@@ -20,6 +20,19 @@ describe('Componente principal', () => {
       render(<App />)
       
       expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+    })
+  })
+
+  describe('Quando eu realizo uma transação', () => {
+    it('que é um saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50
+      }
+
+      const novoSaldo = calcularNovoSaldo(valores, 150)
+
+      expect(novoSaldo).toBe(100)
     })
   })
 })
